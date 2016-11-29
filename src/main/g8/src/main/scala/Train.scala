8a4f9d4e1aa6e1807052011f71b762ce050997c1
Eduardo Gonzalez <me@wm-eddie.com>
2016-11-30 00:21:05 +0900

    Add Spark-based train method.

--- src/main/g8/src/main/scala/Train.scala
+++ src/main/g8/src/main/scala/Train.scala
@@ -24,7 +24,7 @@ import scopt.OptionParser
 import java.io.File
 
 case class TrainConfig(
-  input: File = null,
+  input: String = null,
   modelName: String = "",
   nEpochs: Int = 1
 )
@@ -33,11 +33,11 @@ object TrainConfig {
   val parser = new OptionParser[TrainConfig]("Train") {
       head("$name;format="lower,word"$ Train", "1.0")
 
-      opt[File]('i', "input")
+      opt[String]('i', "input")
         .required()
         .valueName("<file>")
         .action( (x, c) => c.copy(input = x) )
-        .text("The file with training data.")
+        .text("The file/hdfs path with training data.")
 
       opt[Int]('e', "epoch")
         .action( (x, c) => c.copy(nEpochs = x) )
@@ -57,6 +57,16 @@ object TrainConfig {
 
 object Train {
   private val log = LoggerFactory.getLogger(getClass)
+
+  private val irisSchema = StructType(
+    Array(
+      StructField("c1", DoubleType, nullable = false),
+      StructField("c2", DoubleType, nullable = false),
+      StructField("c3", DoubleType, nullable = false),
+      StructField("c4", DoubleType, nullable = false),
+      StructField("label", IntegerType, nullable = false)
+    )
+  )
 
   private def net(nIn: Int, nOut: Int) = new NeuralNetConfiguration.Builder()
     .seed(42)
@@ -95,6 +105,65 @@ object Train {
   }
 
   private def train(c: TrainConfig, sc: SparkContext): Unit = {
-    // TODO
+    val sqlContext = new SQLContext(sc)
+    val csvOptions = Map(
+      "header" -> "false",
+      "inferSchema" -> "false",
+      "delimiter" -> ",",
+      "escape" -> null,
+      "parserLib" -> "univocity",
+      "mode" -> "DROPMALFORMED"
+    )
+    
+    val irisData = sqlContext.load(
+      source = "com.databricks.spark.csv",
+      schema = irisSchema,
+      options = csvOptions + ("path" -> c.input)
+    ).cache()
+
+    irisData.registerTempTable("iris")
+
+
+    val irisPoints = sqlContext.sql(
+      """
+        |SELECT
+        | c1,
+        | c2,
+        | c3,
+        | c4,
+        | label
+        |FROM iris
+      """.stripMargin
+    ).map(row => {
+      LabeledPoint(
+        row.getInt(4), 
+        Vectors.dense(row.getDouble(0), row.getDouble(1), row.getDouble(2), row.getDouble(3))
+      )
+    }).cache()
+
+    val batchSizePerWorker = 10
+
+    val tm = new ParameterAveragingTrainingMaster.Builder(batchSizePerWorker)
+            .averagingFrequency(5)
+            .workerPrefetchNumBatches(2)            //Async prefetching: 2 examples per worker
+            .batchSizePerWorker(batchSizePerWorker)
+            .build()
+
+    //Create the Spark network
+    val conf = net(4, 3)
+    val sparkNet = new SparkDl4jMultiLayer(sc, conf, tm)
+
+       
+    for (i <- 0 until c.nEpochs) {
+      sparkNet.fit(train.toJavaRDD())
+      println(s"Finished epoch $i.")
+    }
+
+    tm.deleteTempFiles(sc)
+
+    ModelSerializer.writeModel(sparkNet.getNetwork, c.modelName, true)
+    normalizer.save((1 to 4).map(j => new File(c.modelName + s".norm$"$"$j")):_*)
+
+    log.info(s"Model saved to: $"$"${c.modelName}")
   }
 }
